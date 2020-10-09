4ff059558f66e6592f634cc38139601ecbe31d7c
Asraf Rosmadi <54825897+asrafrosmadi@users.noreply.github.com>
2020-10-09 12:17:06 +0800

    change

---
+++ index.js
@@ -0,0 +1,7 @@
+/* Responsive Navigation */
+$(document).ready(function(){
+  $('.toggle').click(function(){
+    $('.toggle').toggleClass('active')
+    $('nav ul').toggleClass('active-menu')
+  })
+});
